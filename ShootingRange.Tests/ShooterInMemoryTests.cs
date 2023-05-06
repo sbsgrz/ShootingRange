bbd3711cbc89e06afa53bda213dbc4a5943e1fb0
sbsgrz <sebastian.grzeszczak@hotmail.com>
2023-05-06 15:34:16 +0200

    Koniec

--- ShootingRange.Tests/ShooterInMemoryTests.cs
+++ ShootingRange.Tests/ShooterInMemoryTests.cs
@@ -2,19 +2,70 @@ namespace ShootingRange.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void WhenUserGetPoints_ShouldRemoveCorrectSum()
         {
+            var shooter = new ShooterInMemory();
+
+            shooter.AddResult(0);
+            shooter.AddResult(2);
+            shooter.AddResult(10);
+                     
+            Statistics stats = shooter.GetStatistics();
+            Assert.AreEqual(12, stats.Sum);
         }
 
         [Test]
-        public void Test1()
+        public void WhenUserAddPoints_ShouldRemoveCorrectCount()
         {
-            // assets
-            var s = new ShooterInMemory();
+            var shooter = new ShooterInMemory();
 
-            
-            Assert.Pass();
+            shooter.AddResult(0);
+            shooter.AddResult(2);
+            shooter.AddResult(10);
+
+            Statistics stats = shooter.GetStatistics();
+            Assert.AreEqual(3, stats.Count);
+        }
+
+        [Test]
+        public void WhenUserAddPoints_ShouldRemoveCorrectMax()
+        {
+            var shooter = new ShooterInMemory();
+
+            shooter.AddResult(0);
+            shooter.AddResult(2);
+            shooter.AddResult(10);
+
+            Statistics stats = shooter.GetStatistics();
+            Assert.AreEqual(10, stats.Max);
+        }
+
+        [Test]
+        public void WhenUserAddPoints_ShouldRemoveCorrectMin()
+        {
+            var shooter = new ShooterInMemory();
+
+            shooter.AddResult(0);
+            shooter.AddResult(10);
+
+            Statistics stats = shooter.GetStatistics();
+            Assert.AreEqual(0, stats.Min);
+        }
+
+        [Test]
+        public void WhenUserAddPoints_ShouldRemoveCorrectAvg()
+        {
+            var shooter = new ShooterInMemory();
+
+            shooter.AddResult(0);
+            shooter.AddResult(10);
+            shooter.AddResult(9);
+            shooter.AddResult(5);
+
+
+            Statistics stats = shooter.GetStatistics();
+            Assert.AreEqual(6, stats.Avg);
         }
     }
 }   
